de6ea8ea7ccb8fea931e433c7c99705b324eb27d
robinpernaudat <robin@pernaudat.com>
2016-01-21 20:37:37 +0100

    add empty class JitParser

---
+++ includes/jitparser/JitParser.h
@@ -0,0 +1,21 @@
+/*
+ * JitParser.h
+ *
+ *  Created on: 21 janv. 2016
+ *      Author: robin
+ */
+
+#ifndef JITPARSER_H_
+#define JITPARSER_H_
+
+namespace jit_parser {
+
+class JitParser {
+public:
+	JitParser();
+	virtual ~JitParser();
+};
+
+} /* namespace jit_parser */
+
+#endif /* JITPARSER_H_ */
---
+++ src/JitParser.cpp
@@ -0,0 +1,21 @@
+/*
+ * JitParser.cpp
+ *
+ *  Created on: 21 janv. 2016
+ *      Author: robin
+ */
+
+#include "JitParser.h"
+
+namespace jit_parser {
+
+JitParser::JitParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+JitParser::~JitParser() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace jit_parser */
